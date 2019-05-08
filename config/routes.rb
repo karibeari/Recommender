be044c249acbc5db500c198495f03b87f2d1bf5d
github username <github email address>
2019-05-08 14:16:50 -0600

    remove namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
-  namespace :api do
-    namespace :v1 do
+  # namespace :api do
+  #   namespace :v1 do
       resources :users, only: [:index, :create, :show, :update, :destroy]
       resources :recommendations, only: [:create, :update, :destroy]
-    end
-  end
+    # end
+  # end
 end
