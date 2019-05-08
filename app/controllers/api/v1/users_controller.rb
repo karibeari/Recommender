e3bd47946b48e76ccde11494d4da066654153082
github username <github email address>
2019-05-08 11:50:21 -0600

    find user by name

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,23 +1,28 @@
 class Api::V1::UsersController < ApplicationController
-  # before_action :find_user, only [:show]
 
   def index
     @users = User.all
     render json: @users
   end
 
+  def create
+    @user = User.find_by(name: params[:name])
+    render json: @user
+  end
+
   def show
+    @user = User.find(params[:id])
     render json: @user
   end
 
   private
 
-  def note_params
+  def user_params
     params.permit(:name, :avatar, :hometown)
   end
 
-  def find_user
-    @user = User.find(params[:id])
-  end
+  # def find_user
+  #   @user = User.find(params[:id])
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show]
-      resources :recommendations, only: [:index, :update]
+      resources :users, only: [:index, :create, :show, :update, :destroy]
+      resources :recommendations, only: [:create, :update, :destroy]
     end
   end
 end
